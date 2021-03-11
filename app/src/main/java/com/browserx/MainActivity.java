7df24b2c5ef37240e9e5744586b4ff0011439e29
sravanthi <alivelu9846@gmail.com>
2021-03-11 13:01:02 +0530

    Toast Button Added

--- app/src/main/java/com/browserx/MainActivity.java
+++ app/src/main/java/com/browserx/MainActivity.java
@@ -3,6 +3,9 @@ package com.browserx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_toast=findViewById(R.id.btn_toast);
+        btn_toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "BrowserX app", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
